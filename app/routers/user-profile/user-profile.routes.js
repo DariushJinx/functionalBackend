9597e1e726661762a7b7beb1866d47736a1876e7
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-21 19:40:11 +0330

    update file with update profile

---
+++ app/routers/user-profile/user-profile.routes.js
@@ -0,0 +1,13 @@
+const ProfileController = require("../../http/controllers/user-profile/user-profile.controller");
+
+const ProfileRoutes = require("express").Router();
+
+ProfileRoutes.get("/bookmark-blog", ProfileController.getUserBookmarkedBlogs);
+ProfileRoutes.get(
+  "/bookmark-product",
+  ProfileController.getUserBookmarkedProducts
+);
+ProfileRoutes.get("/basket", ProfileController.getUserBasket);
+ProfileRoutes.patch("/update-profile", ProfileController.updateUserProfile);
+
+module.exports = ProfileRoutes;
