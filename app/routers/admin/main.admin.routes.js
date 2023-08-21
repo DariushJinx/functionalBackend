5b71b4a97be223f9bf9e79b01e083572445a86f8
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-21 19:37:22 +0330

    update file with blog/product/user/comment routes

--- app/routers/admin/main.admin.routes.js
+++ app/routers/admin/main.admin.routes.js
@@ -1,10 +1,16 @@
 const { verifyAccessToken } = require("../../http/middlewares/verifyAccessToken.middleware");
 const BlogRoutes = require("./blog/blog.routes");
 const CategoryRoutes = require("./category/category.routes");
+const commentRoutes = require("./comment/comment.routes");
+const ProductRoutes = require("./product/product.routes");
+const UserRoutes = require("./user/user.routes");
 
 const MainAdminRoutes = require("express").Router();
 
 MainAdminRoutes.use("/category", CategoryRoutes);
-MainAdminRoutes.use("/blog",verifyAccessToken, BlogRoutes);
+MainAdminRoutes.use("/blog", verifyAccessToken, BlogRoutes);
+MainAdminRoutes.use("/product", verifyAccessToken, ProductRoutes);
+MainAdminRoutes.use("/user", verifyAccessToken, UserRoutes);
+MainAdminRoutes.use("/comment", verifyAccessToken, commentRoutes);
 
 module.exports = MainAdminRoutes;
