636a771da3d416b0690fa95e9832612337ec9766
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-21 19:36:26 +0330

    update file with profile/basket/contact routes

--- app/routers/all.routes.js
+++ app/routers/all.routes.js
@@ -1,11 +1,18 @@
+const { verifyAccessToken } = require("../http/middlewares/verifyAccessToken.middleware");
 const MainAdminRoutes = require("./admin/main.admin.routes");
 const AuthRoutes = require("./auth/auth.routes");
+const BasketRoutes = require("./basket/basket.routes");
+const ContactRoutes = require("./contact/contact.routes");
 const HomeRoutes = require("./home/home.routes");
+const ProfileRoutes = require("./user-profile/user-profile.routes");
 
 const AllRoutes = require("express").Router();
 
 AllRoutes.use("/", HomeRoutes);
 AllRoutes.use("/auth", AuthRoutes);
 AllRoutes.use("/admin", MainAdminRoutes);
+AllRoutes.use("/profile",verifyAccessToken, ProfileRoutes);
+AllRoutes.use("/basket",verifyAccessToken, BasketRoutes);
+AllRoutes.use("/contact",verifyAccessToken, ContactRoutes);
 
 module.exports = AllRoutes;
