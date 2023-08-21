4e25c55394b3492256778301f7a8e5948ce56af7
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-21 19:39:07 +0330

    handle routes for basket

---
+++ app/routers/swaggers/swagger/basket.swagger.js
@@ -0,0 +1,52 @@
+/**
+ * @swagger
+ *  /basket/add-product/{productID} :
+ *      patch :
+ *          tags : [Basket]
+ *          summary : add product in basket
+ *          parameters :
+ *              -   in : path
+ *                  name : productID
+ *                  type : string
+ *                  required : true
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/DeleteAndUpdate'
+ */
+/**
+ * @swagger
+ *  /basket/remove-product/{productID} :
+ *      patch :
+ *          tags : [Basket]
+ *          summary : remove product from basket
+ *          parameters :
+ *              -   in : path
+ *                  name : productID
+ *                  type : string
+ *                  required : true
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/DeleteAndUpdate'
+ */
+/**
+ * @swagger
+ *  /basket/remove-all-product :
+ *      patch :
+ *          tags : [Basket]
+ *          summary : remove all products from basket
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content :
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/DeleteAndUpdate'
+ */
