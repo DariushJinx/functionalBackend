92698c8dbe6e19d3180c89a35213972a75179774
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-21 19:38:11 +0330

    handle routes for basket

---
+++ app/routers/basket/basket.routes.js
@@ -0,0 +1,18 @@
+const BasketController = require("../../http/controllers/basket/basket.controller");
+
+const BasketRoutes = require("express").Router();
+
+BasketRoutes.patch(
+  "/add-product/:productID",
+  BasketController.addProductInBasket
+);
+BasketRoutes.patch(
+  "/remove-product/:productID",
+  BasketController.removeProductFromBasket
+);
+BasketRoutes.patch(
+  "/remove-all-product",
+  BasketController.removeAllProductFromBasket
+);
+
+module.exports = BasketRoutes;
