647d97c914ba4534442bd12a84714b152b85069c
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-22 23:28:55 +0330

    file updated with routes about menu and tickets

--- app/routers/all.routes.js
+++ app/routers/all.routes.js
@@ -4,6 +4,8 @@ const AuthRoutes = require("./auth/auth.routes");
 const BasketRoutes = require("./basket/basket.routes");
 const ContactRoutes = require("./contact/contact.routes");
 const HomeRoutes = require("./home/home.routes");
+const MenuRoutes = require("./menu/menu.routes");
+const TicketRoutes = require("./ticket/ticket.routes");
 const ProfileRoutes = require("./user-profile/user-profile.routes");
 
 const AllRoutes = require("express").Router();
@@ -14,5 +16,7 @@ AllRoutes.use("/admin", MainAdminRoutes);
 AllRoutes.use("/profile",verifyAccessToken, ProfileRoutes);
 AllRoutes.use("/basket",verifyAccessToken, BasketRoutes);
 AllRoutes.use("/contact",verifyAccessToken, ContactRoutes);
+AllRoutes.use("/ticket",verifyAccessToken, TicketRoutes);
+AllRoutes.use("/menu", MenuRoutes);
 
 module.exports = AllRoutes;
