ff50ba90821b3109ae549105eae5ce767d86c3b0
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-22 23:29:32 +0330

    file updated with chapter/course/department/...

--- app/routers/admin/main.admin.routes.js
+++ app/routers/admin/main.admin.routes.js
@@ -1,8 +1,16 @@
 const { verifyAccessToken } = require("../../http/middlewares/verifyAccessToken.middleware");
 const BlogRoutes = require("./blog/blog.routes");
 const CategoryRoutes = require("./category/category.routes");
+const ChapterRoutes = require("./chpater/chapter.routes");
 const commentRoutes = require("./comment/comment.routes");
+const CourseRoutes = require("./course/course.routes");
+const DepartmentRoutes = require("./department/department.routes");
+const DepartmentSubRoutes = require("./departmentSub/department-sub.routes");
+const EpisodeRoutes = require("./episode/episode.routes");
+const OffRoutes = require("./off/off.routes");
+const PermissionRoutes = require("./permission/permissioin.routes");
 const ProductRoutes = require("./product/product.routes");
+const RoleRoutes = require("./role/role.routes");
 const UserRoutes = require("./user/user.routes");
 
 const MainAdminRoutes = require("express").Router();
@@ -12,5 +20,13 @@ MainAdminRoutes.use("/blog", verifyAccessToken, BlogRoutes);
 MainAdminRoutes.use("/product", verifyAccessToken, ProductRoutes);
 MainAdminRoutes.use("/user", verifyAccessToken, UserRoutes);
 MainAdminRoutes.use("/comment", verifyAccessToken, commentRoutes);
+MainAdminRoutes.use("/department", DepartmentRoutes);
+MainAdminRoutes.use("/departmentSub", DepartmentSubRoutes);
+MainAdminRoutes.use("/off",verifyAccessToken, OffRoutes);
+MainAdminRoutes.use("/permission",verifyAccessToken, PermissionRoutes);
+MainAdminRoutes.use("/role",verifyAccessToken, RoleRoutes);
+MainAdminRoutes.use("/courses",verifyAccessToken, CourseRoutes);
+MainAdminRoutes.use("/chapter",verifyAccessToken, ChapterRoutes);
+MainAdminRoutes.use("/episode",verifyAccessToken, EpisodeRoutes);
 
 module.exports = MainAdminRoutes;
