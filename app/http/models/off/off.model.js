a512803af01bd26922f41c9d8a6d43b93a2c183a
JinxCyrus <www.dariushkhazaei629@gmail.com>
2023-08-22 23:24:26 +0330

    handle model for off

---
+++ app/http/models/off/off.model.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const OffSchema = new mongoose.Schema(
+  {
+    code: { type: String, required: true },
+    percent: { type: String, required: true },
+    product: { type: mongoose.Types.ObjectId, ref: "products", required: true },
+    max: { type: Number, required: true },
+    uses: { type: Number, required: true },
+    creator: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const OffModel = mongoose.model("offs", OffSchema);
+
+module.exports = OffModel;
