3e6dda8342811d17eaa4d5514f6a2da595515cdd
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 23:48:19 +0330

    file updated for add off for course

--- app/routers/swaggers/adminSwagger/off.swagger.js
+++ app/routers/swaggers/adminSwagger/off.swagger.js
@@ -28,6 +28,32 @@
  * @swagger
  *  components :
  *      schemas :
+ *          CreateOffForCourse :
+ *              type : object
+ *              required :
+ *                  -   code
+ *                  -   percent
+ *                  -   max
+ *                  -   course
+ *              properties :
+ *                  code :
+ *                      type : string
+ *                      description : code for create off
+ *                  percent :
+ *                      type : string
+ *                      description : percent for create off
+ *                  max :
+ *                      type : number
+ *                      description : max for create off
+ *                  course :
+ *                      type : string
+ *                      description : course for create off
+ *
+ */
+/**
+ * @swagger
+ *  components :
+ *      schemas :
  *          GetOne :
  *              type : object
  *              required :
@@ -36,6 +62,20 @@
  *                  product :
  *                      type : string
  *                      description : product for create off
+ *
+ */
+/**
+ * @swagger
+ *  components :
+ *      schemas :
+ *          GetOneOfCourse :
+ *              type : object
+ *              required :
+ *                  -   course
+ *              properties :
+ *                  course :
+ *                      type : string
+ *                      description : course for create off
  *
  */
 /**
@@ -55,7 +95,7 @@
 
 /**
  * @swagger
- *  /admin/off/add :
+ *  /admin/off/add-product :
  *      post :
  *          tags : [Off]
  *          summary : create Off for product
@@ -68,6 +108,29 @@
  *                  application/json :
  *                      schema :
  *                          $ref : '#/components/schemas/Create'
+ *          responses :
+ *              201 :
+ *                  description : create - off created
+ *                  content : 
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/CreateOff'
+ */
+/**
+ * @swagger
+ *  /admin/off/add-course :
+ *      post :
+ *          tags : [Off]
+ *          summary : create Off for course
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/CreateOffForCourse'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/CreateOffForCourse'
  *          responses :
  *              201 :
  *                  description : create - off created
@@ -101,7 +164,7 @@
  */
 /**
  * @swagger
- *  /admin/off/get-one/{code} :
+ *  /admin/off/get-one-product/{code} :
  *      post :
  *          tags : [Off]
  *          summary : get one of product
@@ -127,13 +190,41 @@
  *                          schema :
  *                              $ref : '#/definitions/getOff'
  */
+/**
+ * @swagger
+ *  /admin/off/get-one-course/{code} :
+ *      post :
+ *          tags : [Off]
+ *          summary : get one of course
+ *          parameters :
+ *              -   in : path
+ *                  name : code
+ *                  type : string
+ *                  required : true
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/GetOneOfCourse'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/GetOneOfCourse'
+ *          responses :
+ *              200 :
+ *                  description : success
+ *                  content : 
+ *                      application/json :
+ *                          schema :
+ *                              $ref : '#/definitions/getOff'
+ */
 
 /**
  * @swagger
  *  /admin/off/list :
  *      get :
  *          tags : [Off]
- *          summary : list Off product
+ *          summary : list Off product and course
  *          responses :
  *              200 :
  *                  description : success
@@ -148,7 +239,7 @@
  *  /admin/off/remove/{id} :
  *      delete :
  *          tags : [Off]
- *          summary : remove Off form product
+ *          summary : remove Off form product or course
  *          parameters :
  *              -   in : path
  *                  name : id
