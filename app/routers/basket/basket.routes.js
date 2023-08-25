38c2fb2d2e76e3c286a83dcb434bed9d22b19c92
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 23:47:29 +0330

    file updated with add course in basket

--- app/routers/basket/basket.routes.js
+++ app/routers/basket/basket.routes.js
@@ -2,17 +2,10 @@ const BasketController = require("../../http/controllers/basket/basket.controlle
 
 const BasketRoutes = require("express").Router();
 
-BasketRoutes.patch(
-  "/add-product/:productID",
-  BasketController.addProductInBasket
-);
-BasketRoutes.patch(
-  "/remove-product/:productID",
-  BasketController.removeProductFromBasket
-);
-BasketRoutes.patch(
-  "/remove-all-product",
-  BasketController.removeAllProductFromBasket
-);
+BasketRoutes.patch("/add-product/:productID", BasketController.addProductInBasket);
+BasketRoutes.patch("/remove-product/:productID", BasketController.removeProductFromBasket);
+BasketRoutes.patch("/remove-all-product", BasketController.removeAllProductFromBasket);
+BasketRoutes.patch("/add-course/:courseID", BasketController.addCourseInBasket);
+BasketRoutes.patch("/remove-course/:courseID", BasketController.removeCourseFromBasket);
 
 module.exports = BasketRoutes;
