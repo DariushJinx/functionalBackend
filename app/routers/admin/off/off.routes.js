a66ecd736bd9f28d8396123efe77a0443bbf06c3
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 23:46:27 +0330

    separate add off for product and course

--- app/routers/admin/off/off.routes.js
+++ app/routers/admin/off/off.routes.js
@@ -2,8 +2,10 @@ const OffController = require("../../../http/controllers/off/off.controller");
 
 const OffRoutes = require("express").Router();
 
-OffRoutes.post("/add", OffController.create);
-OffRoutes.post("/get-one/:code", OffController.getOne);
+OffRoutes.post("/add-product", OffController.createForProduct);
+OffRoutes.post("/add-course", OffController.createForCourse);
+OffRoutes.post("/get-one-product/:code", OffController.getOneForProduct);
+OffRoutes.post("/get-one-course/:code", OffController.getOneForCourse);
 OffRoutes.post("/set-all", OffController.setOnAll);
 OffRoutes.get("/list", OffController.getAll);
 OffRoutes.delete("/remove/:id", OffController.removeOff);
