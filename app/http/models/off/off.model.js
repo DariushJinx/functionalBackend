ee2183fbc1dc8f997fce155b703800a24118a3fe
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 23:44:09 +0330

    file updated with adding course

--- app/http/models/off/off.model.js
+++ app/http/models/off/off.model.js
@@ -4,7 +4,8 @@ const OffSchema = new mongoose.Schema(
   {
     code: { type: String, required: true },
     percent: { type: String, required: true },
-    product: { type: mongoose.Types.ObjectId, ref: "products", required: true },
+    product: { type: mongoose.Types.ObjectId, ref: "products" },
+    course: { type: mongoose.Types.ObjectId, ref: "courses" },
     max: { type: Number, required: true },
     uses: { type: Number, required: true },
     creator: { type: mongoose.Types.ObjectId, ref: "users", required: true },
