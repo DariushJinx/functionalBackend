0b5f153c05bab112f3436e64550d8e9c7fe495d5
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 17:06:12 +0330

    separate add comment for blog/product/course

--- app/routers/admin/comment/comment.routes.js
+++ app/routers/admin/comment/comment.routes.js
@@ -1,21 +1,13 @@
 const CommentController = require("../../../http/controllers/comment/comment.controller");
 
-
 const commentRoutes = require("express").Router();
 
-commentRoutes.post("/add", CommentController.createComment);
-commentRoutes.get(
-  "/list",
-  CommentController.getAllComments
-);
-commentRoutes.patch(
-  "/show/:id",
-  CommentController.showComment
-);
+commentRoutes.post("/add-comment-blog", CommentController.createCommentForBlog);
+commentRoutes.post("/add-comment-course", CommentController.createCommentForCourse);
+commentRoutes.post("/add-comment-product", CommentController.createCommentForProduct);
+commentRoutes.get("/list", CommentController.getAllComments);
+commentRoutes.patch("/show/:id", CommentController.showComment);
 commentRoutes.patch("/answer/:id", CommentController.createAnswer);
-commentRoutes.delete(
-  "/remove/:id",
-  CommentController.removeComment
-);
+commentRoutes.delete("/remove/:id", CommentController.removeComment);
 
 module.exports = commentRoutes;
