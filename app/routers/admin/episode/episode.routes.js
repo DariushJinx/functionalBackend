efad3ec6b0b145efb55fa6ad24a7a973c3f2d701
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 17:07:33 +0330

    remove update episode

--- app/routers/admin/episode/episode.routes.js
+++ app/routers/admin/episode/episode.routes.js
@@ -5,9 +5,4 @@ const EpisodeController = require("../../../http/controllers/course/episode.cont
 const EpisodeRoutes = require("express").Router();
 EpisodeRoutes.post("/add", uploadVideo.single("video"), EpisodeController.addNewEpisode);
 EpisodeRoutes.patch("/remove/:episodeID", EpisodeController.removeEpisode);
-EpisodeRoutes.patch(
-  "/update/:episodeID",
-  uploadVideo.single("video"),
-  EpisodeController.updateEpisode
-);
 module.exports = EpisodeRoutes;
