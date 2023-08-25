bf84f50513aafa3da42e839a6943f622ad72720a
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 17:02:10 +0330

    handle course user model and comment model

--- app/http/controllers/course/course.controller.js
+++ app/http/controllers/course/course.controller.js
@@ -10,6 +10,8 @@ const {
 } = require("../../../utils/functions.utils");
 const CourseModel = require("../../models/course/course.model");
 const { default: mongoose } = require("mongoose");
+const CourseUserModel = require("../../models/course-user/course-user.model");
+const CommentModel = require("../../models/comments/comment.model");
 class Course extends Controller {
   async getListOfCourse(req, res, next) {
     try {
@@ -21,18 +23,67 @@ class Course extends Controller {
             { path: "category", select: { title: 1 } },
             { path: "teacher", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
           ])
+          .lean()
           .sort({ _id: -1 });
       else
         courses = await CourseModel.find({})
           .populate([
-            { path: "category", select: { children: 0, prent: 0 } },
+            { path: "category", select: { children: 0, parent: 0, __v: 0 } },
             { path: "teacher", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
           ])
+          .lean()
           .sort({ _id: -1 });
+
+      const registers = await CourseUserModel.find({});
+
+      const comments = await CommentModel.find({ show: 1 })
+        .populate([
+          { path: "commentUser", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
+        ])
+        .lean();
+
+      let allCourses = [];
+      courses.forEach(async (course) => {
+        let courseTotalScore = 5;
+        let courseRegisters = registers.filter((register) => {
+          if (register.course.toString() === course._id.toString()) {
+            return register;
+          }
+        });
+
+        let courseScores = comments.filter((comment) => {
+          if (comment.courseName) {
+            if (comment.courseName.toString() === course._id.toString()) {
+              return comment;
+            }
+          }
+        });
+
+        let courseComments = comments.filter((comment) => {
+          if (comment.courseName) {
+            if (comment.courseName.toString() === course._id.toString()) {
+              return comment;
+            }
+          }
+        });
+
+        courseScores.forEach((comment) => {
+          courseTotalScore += Number(comment.score);
+        });
+
+        allCourses.push({
+          ...course,
+          courseComments,
+          courseAverageScore: Math.floor(courseTotalScore / (courseScores.length + 1)),
+          registers: courseRegisters.length,
+        });
+      });
+
       return res.status(HttpStatus.OK).json({
         statusCode: HttpStatus.OK,
         data: {
-          courses,
+          message: "تمامی دوره های موجود با موفقیت بازگردانی شدند",
+          allCourses,
         },
       });
     } catch (error) {
@@ -72,14 +123,58 @@ class Course extends Controller {
       next(error);
     }
   }
-  async getCourseById(req, res, next) {
+  async getOne(req, res, next) {
     try {
-      const { id } = req.params;
-      const course = await findCourseById(id);
+      const { title } = req.params;
+      const course = await CourseModel.findOne({ title: title })
+        .populate([
+          { path: "category", select: { title: 1 } },
+          { path: "teacher", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
+        ])
+        .lean();
+
+      const comments = await CommentModel.find({ courseName: course._id, show: 1 })
+        .populate([
+          { path: "commentUser", select: { first_name: 1, last_name: 1, mobile: 1, email: 1 } },
+        ])
+        .lean();
+
+      let courseTotalScore = 5;
+
+      let courseScores = comments.filter((comment) => {
+        if (comment.courseName) {
+          if (comment.courseName.toString() === course._id.toString()) {
+            return comment;
+          }
+        }
+      });
+
+      courseScores.forEach((comment) => {
+        courseTotalScore += Number(comment.score);
+      });
+
+      let isUserRegisteredToThisCourse = null;
+      if (req.user) {
+        isUserRegisteredToThisCourse = !!(await CourseUserModel.findOne({
+          user: req.user._id,
+          course: course._id,
+        }));
+      } else {
+        isUserRegisteredToThisCourse = false;
+      }
+
+      const courseStudentsCount = await CourseUserModel.find({
+        course: course._id,
+      }).count();
+
       return res.status(HttpStatus.OK).json({
         statusCode: HttpStatus.OK,
         data: {
-          course,
+          ...course,
+          comments,
+          courseAverageScore: Math.floor(courseTotalScore / (courseScores.length + 1)),
+          isUserRegisteredToThisCourse,
+          courseStudentsCount,
         },
       });
     } catch (error) {
@@ -136,11 +231,43 @@ class Course extends Controller {
       if (!removeResult.deletedCount)
         throw createHttpError.InternalServerError("دوره مورد نظر حذف نشد");
       return res.status(HttpStatus.OK).json({
-        statusCode : HttpStatus.OK,
-        data : {
-          message : "دوره مورد نظر با موفقیت حذف شد"
-        }
-      })
+        statusCode: HttpStatus.OK,
+        data: {
+          message: "دوره مورد نظر با موفقیت حذف شد",
+        },
+      });
+    } catch (err) {
+      next(err);
+    }
+  }
+  async userRegisterCourse(req, res, next) {
+    try {
+      const { courseID } = req.params;
+      const user = req.user;
+      const course = await CourseModel.findOne({ _id: courseID }).lean();
+      const isUserAlreadyRegistered = await CourseUserModel.findOne({
+        user: user._id,
+        course: courseID,
+      }).lean();
+      if (isUserAlreadyRegistered) {
+        return res.status(HttpStatus.CONFLICT).json({
+          statusCode: HttpStatus.CONFLICT,
+          data: {
+            message: "شما قبلا در این دوره ثبت نام کرده اید",
+          },
+        });
+      }
+      await CourseUserModel.create({
+        user: user._id,
+        course: courseID,
+        price: course.price,
+      });
+      return res.status(HttpStatus.CREATED).json({
+        statusCode: HttpStatus.CREATED,
+        data: {
+          message: "شما با موفقیت در دوره ثبت نام کردید",
+        },
+      });
     } catch (err) {
       next(err);
     }
