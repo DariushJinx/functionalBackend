34d1d4e95de0c8cc6fc71b2687ab3d29a96792a5
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-25 17:08:01 +0330

    file updated with separate add comment for P/C/B

--- app/routers/swaggers/adminSwagger/comment.swagger.js
+++ app/routers/swaggers/adminSwagger/comment.swagger.js
@@ -2,20 +2,6 @@
  * @swagger
  *  components :
  *      schemas :
- *          AddComment:
- *              type : object
- *              required :
- *                  -   comment
- *              properties :
- *                  comment :
- *                      type : string
- *                      description : comment for blog or product
- *                  blogName :
- *                      type : string
- *                      description : name of blog
- *                  productName :
- *                      type : string
- *                      description : name of product
  *          UpdateComment:
  *              type : object
  *              required :
@@ -28,19 +14,85 @@
 
 /**
  * @swagger
- *  /admin/comment/add :
+ *  components :
+ *      schemas :
+ *          AddCommentForBlog:
+ *              type : object
+ *              required :
+ *                  -   comment
+ *                  -   score
+ *                  -   blogName
+ *              properties :
+ *                  comment :
+ *                      type : string
+ *                      description : comment for blog
+ *                  score :
+ *                      type : number
+ *                      description : comment score between 1 and 5
+ *                  blogName :
+ *                      type : string
+ *                      description : name of blog
+ */
+
+/**
+ * @swagger
+ *  components :
+ *      schemas :
+ *          AddCommentForCourse:
+ *              type : object
+ *              required :
+ *                  -   comment
+ *                  -   score
+ *                  -   courseName
+ *              properties :
+ *                  comment :
+ *                      type : string
+ *                      description : comment for course
+ *                  score :
+ *                      type : number
+ *                      description : comment score between 1 and 5
+ *                  courseName :
+ *                      type : string
+ *                      description : name of blog
+ */
+
+/**
+ * @swagger
+ *  components :
+ *      schemas :
+ *          AddCommentForProduct:
+ *              type : object
+ *              required :
+ *                  -   comment
+ *                  -   score
+ *                  -   productName
+ *              properties :
+ *                  comment :
+ *                      type : string
+ *                      description : comment for product
+ *                  score :
+ *                      type : number
+ *                      description : comment score between 1 and 5
+ *                  productName :
+ *                      type : string
+ *                      description : name of blog
+ */
+
+/**
+ * @swagger
+ *  /admin/comment/add-comment-blog :
  *      post :
  *          tags : [Comment]
- *          summary : create comment
+ *          summary : create comment for blog
  *          requestBody :
  *              required : true
  *              content :
  *                  application/x-www-form-urlencoded :
  *                      schema :
- *                          $ref : '#/components/schemas/AddComment'
+ *                          $ref : '#/components/schemas/AddCommentForBlog'
  *                  application/json :
  *                      schema :
- *                          $ref : '#/components/schemas/AddComment'
+ *                          $ref : '#/components/schemas/AddCommentForBlog'
  *          responses :
  *              201 :
  *                  description : created
@@ -49,6 +101,55 @@
  *                          schema : 
  *                              $ref : "#/definitions/Comment"
  */
+
+/**
+ * @swagger
+ *  /admin/comment/add-comment-course :
+ *      post :
+ *          tags : [Comment]
+ *          summary : create comment for course
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/AddCommentForCourse'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/AddCommentForCourse'
+ *          responses :
+ *              201 :
+ *                  description : created
+ *                  content : 
+ *                      application/json : 
+ *                          schema : 
+ *                              $ref : "#/definitions/Comment"
+ */
+
+/**
+ * @swagger
+ *  /admin/comment/add-comment-product :
+ *      post :
+ *          tags : [Comment]
+ *          summary : create comment for product
+ *          requestBody :
+ *              required : true
+ *              content :
+ *                  application/x-www-form-urlencoded :
+ *                      schema :
+ *                          $ref : '#/components/schemas/AddCommentForProduct'
+ *                  application/json :
+ *                      schema :
+ *                          $ref : '#/components/schemas/AddCommentForProduct'
+ *          responses :
+ *              201 :
+ *                  description : created
+ *                  content : 
+ *                      application/json : 
+ *                          schema : 
+ *                              $ref : "#/definitions/Comment"
+ */
+
 
 /**
  * @swagger
