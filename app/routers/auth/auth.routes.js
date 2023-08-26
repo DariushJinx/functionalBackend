4e23b398d873e86820caea94eb5edd5c7be89262
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-26 19:04:17 +0330

    file updated with me route

--- app/routers/auth/auth.routes.js
+++ app/routers/auth/auth.routes.js
@@ -1,4 +1,5 @@
 const AuthController = require("../../http/controllers/auth/auth.controller");
+const { verifyAccessToken } = require("../../http/middlewares/verifyAccessToken.middleware");
 
 const AuthRoutes = require("express").Router();
 
@@ -6,6 +7,6 @@ AuthRoutes.post("/get-otp", AuthController.getOtp);
 AuthRoutes.post("/check-otp", AuthController.checkOtp);
 AuthRoutes.post("/register", AuthController.register);
 AuthRoutes.post("/login", AuthController.login);
-
+AuthRoutes.get("/me",verifyAccessToken, AuthController.getMe);
 
 module.exports = AuthRoutes;
