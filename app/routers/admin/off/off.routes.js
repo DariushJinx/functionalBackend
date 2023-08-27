463faac2b9efce684eb0b14c962cb2057e128b93
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:14:34 +0330

    use verifyAccessToken and permissions

--- app/routers/admin/off/off.routes.js
+++ app/routers/admin/off/off.routes.js
@@ -1,13 +1,27 @@
 const OffController = require("../../../http/controllers/off/off.controller");
+const checkPermission = require("../../../http/middlewares/permission.guard");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
 
 const OffRoutes = require("express").Router();
 
-OffRoutes.post("/add-product", OffController.createForProduct);
-OffRoutes.post("/add-course", OffController.createForCourse);
-OffRoutes.post("/get-one-product/:code", OffController.getOneForProduct);
-OffRoutes.post("/get-one-course/:code", OffController.getOneForCourse);
-OffRoutes.post("/set-all", OffController.setOnAll);
-OffRoutes.get("/list", OffController.getAll);
-OffRoutes.delete("/remove/:id", OffController.removeOff);
+OffRoutes.post(
+  "/add-product",
+  checkPermission([PERMISSIONS.ADMIN]),
+  OffController.createForProduct
+);
+OffRoutes.post("/add-course", checkPermission([PERMISSIONS.ADMIN]), OffController.createForCourse);
+OffRoutes.post(
+  "/get-one-product/:code",
+  checkPermission([PERMISSIONS.ADMIN]),
+  OffController.getOneForProduct
+);
+OffRoutes.post(
+  "/get-one-course/:code",
+  checkPermission([PERMISSIONS.ADMIN]),
+  OffController.getOneForCourse
+);
+OffRoutes.post("/set-all", checkPermission([PERMISSIONS.ADMIN]), OffController.setOnAll);
+OffRoutes.get("/list", checkPermission([PERMISSIONS.ADMIN]), OffController.getAll);
+OffRoutes.delete("/remove/:id", checkPermission([PERMISSIONS.ADMIN]), OffController.removeOff);
 
 module.exports = OffRoutes;
