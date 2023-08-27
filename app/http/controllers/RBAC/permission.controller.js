6b495e0f88e5aea29905c9898bc50948838a8676
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:11:31 +0330

    file updated with remove errors & add status json

--- app/http/controllers/RBAC/permission.controller.js
+++ app/http/controllers/RBAC/permission.controller.js
@@ -1,21 +1,23 @@
-const createHttpError = require("http-errors");
 const PermissionModel = require("../../models/permission/permission.model");
 const PermissionValidation = require("../../validation/RBAC/permission.validation");
 const { StatusCodes: HttpStatus } = require("http-status-codes");
-const {
-  copyObject,
-  deleteInvalidPropertyInObject,
-} = require("../../../utils/functions.utils");
+const { copyObject, deleteInvalidPropertyInObject } = require("../../../utils/functions.utils");
 
-exports.createNewPermission = async(req, res, next) => {
+exports.createNewPermission = async (req, res, next) => {
   try {
     const validation = await PermissionValidation.validateAsync(req.body);
     const { name, description } = validation;
     await findPermissionWithName(name);
     const permissions = await PermissionModel.create({ name, description });
-    if (!permissions)
-      throw createHttpError.InternalServerError("دسترسی مورد نظر ایجاد نشد");
-    res.status(HttpStatus.CREATED).json({
+    if (!permissions) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "دسترسی مورد نظر ایجاد نشد",
+        },
+      });
+    }
+    return res.status(HttpStatus.CREATED).json({
       statusCode: HttpStatus.CREATED,
       data: {
         message: "دسترسی مورد نظر ایجاد شد",
@@ -25,9 +27,9 @@ exports.createNewPermission = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
-exports.getAllPermission = async(req, res, next) => {
+exports.getAllPermission = async (req, res, next) => {
   try {
     const permissions = await PermissionModel.find({});
     return res.status(HttpStatus.OK).json({
@@ -40,15 +42,21 @@ exports.getAllPermission = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
-exports.removePermission = async(req, res, next) => {
+exports.removePermission = async (req, res, next) => {
   try {
     const { id } = req.params;
     await findPermissionWithID(id);
     const removeResult = await PermissionModel.deleteOne({ _id: id });
-    if (!removeResult.deletedCount)
-      throw createHttpError.InternalServerError("دسترسی مورد نظر حذف نشد");
+    if (!removeResult.deletedCount) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "دسترسی مورد نظر حذف نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -58,22 +66,23 @@ exports.removePermission = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
-exports.updatePermission = async(req, res, next) => {
+exports.updatePermission = async (req, res, next) => {
   try {
     const { id } = req.params;
     const permission = await findPermissionWithID(id);
     const data = copyObject(req.body);
     deleteInvalidPropertyInObject(data, []);
-    const updateResult = await PermissionModel.updateOne(
-      { _id: permission._id },
-      { $set: data }
-    );
-    if (!updateResult.modifiedCount)
-      throw createHttpError.InternalServerError(
-        "به روزرسانی دسترسی مورد نظر با موفقیت انجام نشد"
-      );
+    const updateResult = await PermissionModel.updateOne({ _id: permission._id }, { $set: data });
+    if (!updateResult.modifiedCount) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "به روزرسانی دسترسی مورد نظر با موفقیت انجام نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -83,16 +92,29 @@ exports.updatePermission = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 const findPermissionWithID = async (ID) => {
   const permission = await PermissionModel.findById(ID);
-  if (!permission) throw createHttpError.NotFound("دسترسی مورد نظر یافت نشد");
+  if (!permission) {
+    return res.status(HttpStatus.NOT_FOUND).json({
+      statusCode: HttpStatus.NOT_FOUND,
+      data: {
+        message: "دسترسی مورد نظر یافت نشد",
+      },
+    });
+  }
   return permission;
-}
+};
 
 const findPermissionWithName = async (name) => {
   const permission = await PermissionModel.findOne({ name });
-  if (permission)
-    throw createHttpError.BadRequest("دسترسی مورد نظر از قبل ایجاد شده است");
-}
+  if (permission) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      statusCode: HttpStatus.NOT_FOUND,
+      data: {
+        message: "دسترسی مورد نظر از قبل ایجاد شده است",
+      },
+    });
+  }
+};
