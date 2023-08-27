38a5ca79c81544cb25a2a7aba168aecb30f9b590
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:11:39 +0330

    file updated with remove errors & add status json

--- app/http/controllers/RBAC/role.controller.js
+++ app/http/controllers/RBAC/role.controller.js
@@ -1,23 +1,24 @@
-const createHttpError = require("http-errors");
 const RoleModel = require("../../models/role/role.model");
 const RoleValidation = require("../../validation/RBAC/role.validation");
 const { StatusCodes: HttpStatus } = require("http-status-codes");
 const { default: mongoose } = require("mongoose");
-const {
-  copyObject,
-  deleteInvalidPropertyInObject,
-} = require("../../../utils/functions.utils");
+const { copyObject, deleteInvalidPropertyInObject } = require("../../../utils/functions.utils");
 
-exports.createNewRole = async(req, res, next) => {
+exports.createNewRole = async (req, res, next) => {
   try {
     const validation = await RoleValidation.validateAsync(req.body);
     const { title, description, permissions } = validation;
     await findRoleWithTitle(title);
     const roles = await RoleModel.create({ title, description, permissions });
-    if (!roles)
-      throw createHttpError.InternalServerError(
-        "نقش مورد نظر با موفقیت ایجاد نشد"
-      );
+    if (!roles) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "نقش مورد نظر با موفقیت ایجاد نشد",
+          roles,
+        },
+      });
+    }
     return res.status(HttpStatus.CREATED).json({
       statusCode: HttpStatus.CREATED,
       data: {
@@ -28,7 +29,7 @@ exports.createNewRole = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 exports.getAllRoles = async (req, res, next) => {
   try {
@@ -43,17 +44,21 @@ exports.getAllRoles = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
-exports.removeRole = async(req, res, next) => {
+exports.removeRole = async (req, res, next) => {
   try {
     const { field } = req.params;
     const role = await findRoleWithTitleOrID(field);
     const removeResult = await RoleModel.deleteOne({ _id: role._id });
-    if (!removeResult.deletedCount)
-      throw createHttpError.InternalServerError(
-        "حذف نقش با موفقیت انجام نشد"
-      );
+    if (!removeResult.deletedCount) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "حذف نقش با موفقیت انجام نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -63,22 +68,23 @@ exports.removeRole = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
-exports.updateRole = async(req, res, next) => {
+exports.updateRole = async (req, res, next) => {
   try {
     const { field } = req.params;
     const role = await findRoleWithTitleOrID(field);
     const data = copyObject(req.body);
     deleteInvalidPropertyInObject(data, []);
-    const updateResult = await RoleModel.updateOne(
-      { _id: role._id },
-      { $set: data }
-    );
-    if (!updateResult.modifiedCount)
-      throw createHttpError.InternalServerError(
-        "نقش مورد نظر با موفقیت به روزرسانی نشد"
-      );
+    const updateResult = await RoleModel.updateOne({ _id: role._id }, { $set: data });
+    if (!updateResult.modifiedCount) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "نقش مورد نظر با موفقیت به روزرسانی نشد",
+        },
+      });
+    }
     res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK.toExponential,
       data: {
@@ -88,19 +94,30 @@ exports.updateRole = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 const findRoleWithTitleOrID = async (field) => {
-  const findQuery = mongoose.isValidObjectId(field)
-    ? { _id: field }
-    : { title: field };
+  const findQuery = mongoose.isValidObjectId(field) ? { _id: field } : { title: field };
   const role = await RoleModel.findOne(findQuery);
-  if (!role) throw createHttpError.NotFound("نقش مورد نظر یافت نشد");
+  if (!role) {
+    return res.status(HttpStatus.NOT_FOUND).json({
+      statusCode: HttpStatus.NOT_FOUND,
+      data: {
+        message: "نقش مورد نظر یافت نشد",
+      },
+    });
+  }
   return role;
-}
+};
 
 const findRoleWithTitle = async (title) => {
   const role = await RoleModel.findOne({ title });
-  if (role)
-    throw createHttpError.BadRequest("نقش مورد نظر قبلا ایجاد شده است");
-}
+  if (role) {
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      statusCode: HttpStatus.BAD_REQUEST,
+      data: {
+        message: "نقش مورد نظر قبلا ایجاد شده است",
+      },
+    });
+  }
+};
