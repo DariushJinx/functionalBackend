47e10c5a61b7c9bacf25c70892eb7d2227d1743a
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:14:58 +0330

    use verifyAccessToken and permissions

--- app/routers/admin/role/role.routes.js
+++ app/routers/admin/role/role.routes.js
@@ -1,18 +1,26 @@
 const RoleController = require("../../../http/controllers/RBAC/role.controller");
+const checkPermission = require("../../../http/middlewares/permission.guard");
 
 const stringToArray = require("../../../http/middlewares/stringToArray");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
 
 const RoleRoutes = require("express").Router();
 
 RoleRoutes.post(
   "/add",
+  checkPermission([PERMISSIONS.ADMIN]),
   stringToArray("permissions"),
   RoleController.createNewRole
 );
-RoleRoutes.get("/list", RoleController.getAllRoles);
-RoleRoutes.delete("/remove/:field", RoleController.removeRole);
+RoleRoutes.get("/list", checkPermission([PERMISSIONS.ADMIN]), RoleController.getAllRoles);
+RoleRoutes.delete(
+  "/remove/:field",
+  checkPermission([PERMISSIONS.ADMIN]),
+  RoleController.removeRole
+);
 RoleRoutes.patch(
   "/update/:field",
+  checkPermission([PERMISSIONS.ADMIN]),
   stringToArray("permissions"),
   RoleController.updateRole
 );
