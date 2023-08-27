72831091c237c684d4c66a93633fe7f9c99892f4
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:15:12 +0330

    use verifyAccessToken and permissions

--- app/routers/auth/auth.routes.js
+++ app/routers/auth/auth.routes.js
@@ -1,5 +1,7 @@
 const AuthController = require("../../http/controllers/auth/auth.controller");
+const checkPermission = require("../../http/middlewares/permission.guard");
 const { verifyAccessToken } = require("../../http/middlewares/verifyAccessToken.middleware");
+const { PERMISSIONS } = require("../../utils/constans.utils");
 
 const AuthRoutes = require("express").Router();
 
@@ -7,6 +9,11 @@ AuthRoutes.post("/get-otp", AuthController.getOtp);
 AuthRoutes.post("/check-otp", AuthController.checkOtp);
 AuthRoutes.post("/register", AuthController.register);
 AuthRoutes.post("/login", AuthController.login);
-AuthRoutes.get("/me",verifyAccessToken, AuthController.getMe);
+AuthRoutes.get(
+  "/me",
+  verifyAccessToken,
+  checkPermission([PERMISSIONS.ADMIN]),
+  AuthController.getMe
+);
 
 module.exports = AuthRoutes;
