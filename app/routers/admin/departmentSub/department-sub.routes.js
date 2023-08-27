7726b6953d4e22c97bd6aee59046e9593cb51b46
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:13:59 +0330

    use verifyAccessToken and permissions

--- app/routers/admin/departmentSub/department-sub.routes.js
+++ app/routers/admin/departmentSub/department-sub.routes.js
@@ -1,15 +1,27 @@
 const DepartmentSubController = require("../../../http/controllers/departmentSub/department-sub.controller");
+const checkPermission = require("../../../http/middlewares/permission.guard");
+const { verifyAccessToken } = require("../../../http/middlewares/verifyAccessToken.middleware");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
 
 const DepartmentSubRoutes = require("express").Router();
 
-DepartmentSubRoutes.post("/add", DepartmentSubController.createDepartmentSub);
+DepartmentSubRoutes.post(
+  "/add",
+  verifyAccessToken,
+  checkPermission([PERMISSIONS.ADMIN]),
+  DepartmentSubController.createDepartmentSub
+);
 DepartmentSubRoutes.get("/list", DepartmentSubController.listOfDepartmentSub);
 DepartmentSubRoutes.patch(
   "/update/:field",
+  verifyAccessToken,
+  checkPermission([PERMISSIONS.ADMIN]),
   DepartmentSubController.updateDepartmentSub
 );
 DepartmentSubRoutes.delete(
   "/remove/:field",
+  verifyAccessToken,
+  checkPermission([PERMISSIONS.ADMIN]),
   DepartmentSubController.removeDepartmentSub
 );
 
