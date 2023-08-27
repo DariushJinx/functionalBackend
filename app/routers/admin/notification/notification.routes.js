5a848c0eb2c18a092643a1a280cc430c2d7bbfbd
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:14:29 +0330

    use verifyAccessToken and permissions

--- app/routers/admin/notification/notification.routes.js
+++ app/routers/admin/notification/notification.routes.js
@@ -1,7 +1,21 @@
 const NotificationRoutes = require("express").Router();
 const NotificationController = require("../../../http/controllers/notification/notification.controller");
-NotificationRoutes.post("/add", NotificationController.create);
-NotificationRoutes.get("/all-notifications", NotificationController.getAllNotifications);
-NotificationRoutes.patch("/see/:id", NotificationController.see);
+const checkPermission = require("../../../http/middlewares/permission.guard");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
+NotificationRoutes.post(
+  "/add",
+  checkPermission([PERMISSIONS.ADMIN]),
+  NotificationController.create
+);
+NotificationRoutes.get(
+  "/all-notifications",
+  checkPermission([PERMISSIONS.ADMIN]),
+  NotificationController.getAllNotifications
+);
+NotificationRoutes.patch(
+  "/see/:id",
+  checkPermission([PERMISSIONS.ADMIN]),
+  NotificationController.see
+);
 
 module.exports = NotificationRoutes;
