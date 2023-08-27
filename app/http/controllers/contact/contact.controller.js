2cfd8c753561a6c325058b710ad77414b209f622
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:10:06 +0330

    file updated with remove errors & add status json

--- app/http/controllers/contact/contact.controller.js
+++ app/http/controllers/contact/contact.controller.js
@@ -1,4 +1,3 @@
-const createHttpError = require("http-errors");
 const ContactModel = require("../../models/contact/contact.model");
 const {
   CreateContactValidation,
@@ -8,7 +7,7 @@ const {
 const { StatusCodes: HttpStatus } = require("http-status-codes");
 const nodemailer = require("nodemailer");
 
-exports.createContact = async(req, res, next) => {
+exports.createContact = async (req, res, next) => {
   try {
     const validation = await CreateContactValidation.validateAsync(req.body);
     const { name, email, phone, body } = validation;
@@ -19,8 +18,15 @@ exports.createContact = async(req, res, next) => {
       body,
       answer: false,
     });
-    if (!contact)
-      throw createHttpError.InternalServerError("ارتباط با موفقیت ایجاد نشد");
+    if (!contact) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "ارتباط با موفقیت ایجاد نشد",
+          contact,
+        },
+      });
+    }
     return res.status(HttpStatus.CREATED).json({
       statusCode: HttpStatus.CREATED,
       data: {
@@ -31,8 +37,8 @@ exports.createContact = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
-exports.getAllContact = async(req, res, next) => {
+};
+exports.getAllContact = async (req, res, next) => {
   try {
     const contacts = await ContactModel.find({});
     return res.status(HttpStatus.OK).json({
@@ -45,14 +51,20 @@ exports.getAllContact = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
-exports.removeContact = async(req, res, next) => {
+};
+exports.removeContact = async (req, res, next) => {
   try {
     const validation = await RemoveValidation.validateAsync(req.params);
     const { id } = validation;
     const removeResult = await ContactModel.findOneAndRemove({ _id: id });
-    if (!removeResult)
-      throw createHttpError.NotFound("ارتباط مورد نظر یافت نشد");
+    if (!removeResult) {
+      return res.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
+        data: {
+          message: "ارتباط مورد نظر یافت نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -62,8 +74,8 @@ exports.removeContact = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
-exports.answerContact = async(req, res, next) => {
+};
+exports.answerContact = async (req, res, next) => {
   try {
     const validation = await AnswerValidation.validateAsync(req.body);
     const { answer, email } = validation;
@@ -80,10 +92,7 @@ exports.answerContact = async(req, res, next) => {
       subject: "پاسخ ایمیل شما از طرف سایت فلان",
       text: answer,
     };
-    const contact = await ContactModel.findOneAndUpdate(
-      { email: email },
-      { answer: true }
-    );
+    const contact = await ContactModel.findOneAndUpdate({ email: email }, { answer: true });
     transporter.sendMail(mailOptions, function (error, info) {
       if (error) {
         res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
@@ -105,4 +114,4 @@ exports.answerContact = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
