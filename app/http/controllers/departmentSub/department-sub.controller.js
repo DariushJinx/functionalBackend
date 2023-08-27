92b849e6f23e5408f051412ae78b282d7c129fd1
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:10:58 +0330

    file updated with remove errors & add status json

--- app/http/controllers/departmentSub/department-sub.controller.js
+++ app/http/controllers/departmentSub/department-sub.controller.js
@@ -1,20 +1,22 @@
-const createHttpError = require("http-errors");
 const DepartmentSubModel = require("../../models/department-sub/department-sub.model");
-const {
-  departmentSubValidation,
-} = require("../../validation/department/department.validation");
+const { departmentSubValidation } = require("../../validation/department/department.validation");
 const { StatusCodes: HttpStatus } = require("http-status-codes");
 const { default: mongoose } = require("mongoose");
 
-exports.createDepartmentSub = async(req, res, next) => {
+exports.createDepartmentSub = async (req, res, next) => {
   try {
     const validation = await departmentSubValidation.validateAsync(req.body);
     const { title, parent } = validation;
     const departmentSub = await DepartmentSubModel.create({ title, parent });
-    if (!departmentSub)
-      throw createHttpError.InternalServerError(
-        "زیر دپارتمانت مورد نظر ایجاد نشد"
-      );
+    if (!departmentSub) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "زیر دپارتمانت مورد نظر ایجاد نشد",
+        },
+      });
+    }
+
     return res.status(HttpStatus.CREATED).json({
       statusCode: HttpStatus.CREATED,
       data: {
@@ -25,18 +27,22 @@ exports.createDepartmentSub = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
-exports.removeDepartmentSub = async(req, res, next) => {
+};
+exports.removeDepartmentSub = async (req, res, next) => {
   try {
     const { field } = req.params;
     const departmentSub = await findDepartment(field);
     const removeResult = await DepartmentSubModel.deleteOne({
       _id: departmentSub._id,
     });
-    if (!removeResult.deletedCount)
-      throw createHttpError.InternalServerError(
-        "زیر دپارتمانت مورد نظر حذف نشد"
-      );
+    if (!removeResult.deletedCount) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "زیر دپارتمانت مورد نظر حذف نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -46,8 +52,8 @@ exports.removeDepartmentSub = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
-exports.updateDepartmentSub = async(req, res, next) => {
+};
+exports.updateDepartmentSub = async (req, res, next) => {
   try {
     const { field } = req.params;
     const { title } = req.body;
@@ -60,10 +66,14 @@ exports.updateDepartmentSub = async(req, res, next) => {
         $set: { title },
       }
     );
-    if (!updateResult.modifiedCount)
-      throw createHttpError.InternalServerError(
-        "زیر دپارتمانت مورد نظر به روزرسانی نشد"
-      );
+    if (!updateResult.modifiedCount) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        data: {
+          message: "زیر دپارتمانت مورد نظر به روزرسانی نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -73,14 +83,20 @@ exports.updateDepartmentSub = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
-exports.listOfDepartmentSub = async(req, res, next) => {
+};
+exports.listOfDepartmentSub = async (req, res, next) => {
   try {
-    const list = await DepartmentSubModel.find({}).populate([
-      { path: "parent" , select : {title : 1,_id:0} },
-    ]).lean()
-    if (!list)
-      throw createHttpError.NotFound("لیستی از زیر دپارتمانت ها یافت نشد");
+    const list = await DepartmentSubModel.find({})
+      .populate([{ path: "parent", select: { title: 1, _id: 0 } }])
+      .lean();
+    if (!list) {
+      return res.status(HttpStatus.NOT_FOUND).json({
+        statusCode: HttpStatus.NOT_FOUND,
+        data: {
+          message: "لیستی از زیر دپارتمانت ها یافت نشد",
+        },
+      });
+    }
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -91,14 +107,18 @@ exports.listOfDepartmentSub = async(req, res, next) => {
   } catch (err) {
     next(err);
   }
-}
+};
 
 async function findDepartment(field) {
-  const findQuery = mongoose.isValidObjectId(field)
-    ? { _id: field }
-    : { title: field };
+  const findQuery = mongoose.isValidObjectId(field) ? { _id: field } : { title: field };
   const departmentSub = await DepartmentSubModel.findOne(findQuery);
-  if (!departmentSub)
-    throw createHttpError.NotFound("دپارتمانت مورد نظر یافت نشد");
+  if (!departmentSub) {
+    return res.status(HttpStatus.NOT_FOUND).json({
+      statusCode: HttpStatus.NOT_FOUND,
+      data: {
+        message: "دپارتمانت مورد نظر یافت نشد",
+      },
+    });
+  }
   return departmentSub;
 }
