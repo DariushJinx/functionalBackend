8290d451eb420e0dfa5b29b6ac72773f329155cf
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:13:30 +0330

    use verifyAccessToken and permissions

--- app/routers/admin/chpater/chapter.routes.js
+++ app/routers/admin/chpater/chapter.routes.js
@@ -1,8 +1,10 @@
 const ChapterController = require("../../../http/controllers/course/chapter.controller");
+const checkPermission = require("../../../http/middlewares/permission.guard");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
 
 const ChapterRoutes = require("express").Router();
-ChapterRoutes.put("/add", ChapterController.addChapter); //create new chapter
+ChapterRoutes.put("/add",checkPermission([PERMISSIONS.ADMIN]), ChapterController.addChapter); //create new chapter
 ChapterRoutes.get("/list/:courseID", ChapterController.chaptersOfCourse); //create new chapter
-ChapterRoutes.patch("/remove/:chapterID", ChapterController.removeChapterById); //create new chapter
-ChapterRoutes.patch("/update/:chapterID", ChapterController.updateChapterById); //create new chapter
+ChapterRoutes.patch("/remove/:chapterID",checkPermission([PERMISSIONS.ADMIN]), ChapterController.removeChapterById); //create new chapter
+ChapterRoutes.patch("/update/:chapterID",checkPermission([PERMISSIONS.ADMIN]), ChapterController.updateChapterById); //create new chapter
 module.exports = ChapterRoutes;
