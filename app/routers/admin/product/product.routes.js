8a0004ecf24644bc9096ccc411158b8b107e3ba5
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-27 13:14:50 +0330

    use verifyAccessToken and permissions

--- app/routers/admin/product/product.routes.js
+++ app/routers/admin/product/product.routes.js
@@ -1,22 +1,38 @@
 const ProductController = require("../../../http/controllers/product/product.controller");
+const checkPermission = require("../../../http/middlewares/permission.guard");
 const stringToArray = require("../../../http/middlewares/stringToArray");
+const { PERMISSIONS } = require("../../../utils/constans.utils");
 const { uploadFile } = require("../../../utils/multer.utils");
 
 const ProductRoutes = require("express").Router();
 
 ProductRoutes.post(
   "/add",
+  checkPermission([PERMISSIONS.ADMIN]),
   uploadFile.array("images", 10),
   stringToArray("tags", "colors"),
   ProductController.addProduct
 );
 ProductRoutes.get("/list", ProductController.listOfProduct);
 ProductRoutes.get("/search", ProductController.searchOfProduct);
-ProductRoutes.patch("/add-features/:field", ProductController.addFeaturesForProduct);
-ProductRoutes.patch("/remove-feature/:productID", ProductController.removeFeature);
-ProductRoutes.delete("/remove-product/:field", ProductController.removeProduct);
+ProductRoutes.patch(
+  "/add-features/:field",
+  checkPermission([PERMISSIONS.ADMIN]),
+  ProductController.addFeaturesForProduct
+);
+ProductRoutes.patch(
+  "/remove-feature/:productID",
+  checkPermission([PERMISSIONS.ADMIN]),
+  ProductController.removeFeature
+);
+ProductRoutes.delete(
+  "/remove-product/:field",
+  checkPermission([PERMISSIONS.ADMIN]),
+  ProductController.removeProduct
+);
 ProductRoutes.patch(
   "/update-product/:field",
+  checkPermission([PERMISSIONS.ADMIN]),
   uploadFile.array("images", 10),
   stringToArray("tags", "colors"),
   ProductController.updateProduct
