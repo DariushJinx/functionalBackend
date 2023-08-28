a9551206355e9433974a55e00ce781f8eb3901be
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-28 17:15:00 +0330

    update file with handle route for support

--- app/routers/all.routes.js
+++ app/routers/all.routes.js
@@ -5,6 +5,7 @@ const BasketRoutes = require("./basket/basket.routes");
 const ContactRoutes = require("./contact/contact.routes");
 const HomeRoutes = require("./home/home.routes");
 const MenuRoutes = require("./menu/menu.routes");
+const supportSectionRoutes = require("./support/support.routes");
 const TicketRoutes = require("./ticket/ticket.routes");
 const ProfileRoutes = require("./user-profile/user-profile.routes");
 
@@ -18,5 +19,6 @@ AllRoutes.use("/basket", verifyAccessToken, BasketRoutes);
 AllRoutes.use("/contact", verifyAccessToken, ContactRoutes);
 AllRoutes.use("/ticket", verifyAccessToken, TicketRoutes);
 AllRoutes.use("/menu", MenuRoutes);
+AllRoutes.use("/support", supportSectionRoutes);
 
 module.exports = AllRoutes;
