b10ef3252336dd99ab1ca45c625f9e5ce8ec6254
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-28 17:11:15 +0330

    update file with handle cookie template engine

--- app/server.js
+++ app/server.js
@@ -7,11 +7,20 @@ const path = require("path");
 const { default: mongoose } = require("mongoose");
 const AllRoutes = require("./routers/all.routes");
 const { notFound, errors } = require("./utils/erroHandler.utils");
+const cookieParser = require("cookie-parser");
+const { COOKIE_PARSER_SECRET_KEY } = require("./utils/constans.utils");
+const session = require("express-session");
+const expressEjsLayouts = require("express-ejs-layouts");
+const clientHelper = require("./utils/client.utils");
+const { socketHandler } = require("./socket.io");
+const initialSocket = require("./utils/initSocket.utils");
 const app = express();
 require("dotenv").config();
 
 module.exports = function Application(DB_URI, PORT) {
   configApplication();
+  initTemplateEngine();
+  initClientSession();
   connectToMongoDB(DB_URI);
   createServer(PORT);
   createRoutes();
@@ -79,6 +88,8 @@ function connectToMongoDB(uri) {
 function createServer(port) {
   const http = require("http");
   const server = http.createServer(app);
+  const io = initialSocket(server);
+  socketHandler(io);
   server.listen(port, () => {
     console.log(`server run on => http://127.0.0.1:${port}`);
   });
@@ -94,5 +105,33 @@ function errorHandler() {
   });
   app.use((error, req, res, next) => {
     errors(error, res);
+  });
+}
+
+function initClientSession() {
+  app.use(cookieParser(COOKIE_PARSER_SECRET_KEY));
+  app.use(
+    session({
+      secret: COOKIE_PARSER_SECRET_KEY,
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        secure: true,
+        signed: true,
+      },
+    })
+  );
+}
+
+function initTemplateEngine() {
+  app.use(expressEjsLayouts);
+  app.set("view engine", "ejs");
+  app.set("views", "resource/views");
+  app.set("layout extractStyles", true);
+  app.set("layout extractScripts", true);
+  app.set("layout", "./layouts/master");
+  app.use((req, res, next) => {
+    app.locals = clientHelper(req, res);
+    next();
   });
 }
