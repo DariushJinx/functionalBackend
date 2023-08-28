b9a0d0b3a6e245b4daf8ba5eacd069506b3d6f3e
DariushJinx <106846875+DariushJinx@users.noreply.github.com>
2023-08-28 17:18:24 +0330

    handle setting for socket

---
+++ app/utils/initSocket.utils.js
@@ -0,0 +1,12 @@
+const socketIO = require("socket.io");
+function initialSocket(httpServer) {
+  const io = socketIO(httpServer, {
+    cors: {
+      origin: "*",
+    },
+    maxHttpBufferSize: 1e8,
+  });
+  return io;
+}
+
+module.exports = initialSocket;
