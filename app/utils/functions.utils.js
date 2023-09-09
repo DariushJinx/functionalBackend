6363ef0096b6928113352796a9e1a78cc7fb45a5
Dariush <106846875+DariushJinx@users.noreply.github.com>
2023-09-09 18:56:37 +0330

    Update functions.utils.js for images

--- app/utils/functions.utils.js
+++ app/utils/functions.utils.js
@@ -72,7 +72,7 @@ function ListOfImagesForRequest(files, fileUploadPath) {
           file.filename
         )
       )
-      .map((item) => item.replace(/\\/g, "/"));
+      .map((item) => item.replace(/\\/g, "//"));
   } else {
     return [];
   }
